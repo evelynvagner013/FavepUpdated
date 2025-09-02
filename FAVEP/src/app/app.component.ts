4cb8dd1292683b0ce207d8cd5c1403f5e0e15eff
evelynvagner013 <evelynvagner2006@gmail.com>
2025-09-02 16:41:45 -0300

    02/09 17h

--- FAVEP/src/app/app.component.ts
+++ FAVEP/src/app/app.component.ts
@@ -1,21 +1,46 @@
 import { Component } from '@angular/core'; 
 import { RouterOutlet } from '@angular/router';
 import { VLibrasService } from '../services/vlibras.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet 
+    RouterOutlet,
+     CommonModule 
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent { 
+   showAccessibilityOptions: boolean = false;
+
   constructor(private vlibras: VLibrasService) {}
 
   ngOnInit() {
     this.vlibras.initVLibras();
 }
+ private readonly maxFontSize = 1.4; // 140%
+  private readonly minFontSize = 0.8; // 80%
+  private readonly step = 0.1; // 10%
+
+  aumentarFonte() {
+    let currentFontSize = parseFloat(document.documentElement.style.fontSize || '1');
+    if (currentFontSize < this.maxFontSize) {
+      document.documentElement.style.fontSize = `${currentFontSize + this.step}rem`;
+    }
+  }
+
+  diminuirFonte() {
+    let currentFontSize = parseFloat(document.documentElement.style.fontSize || '1');
+    if (currentFontSize > this.minFontSize) {
+      document.documentElement.style.fontSize = `${currentFontSize - this.step}rem`;
+    }
+  }
+
+  toggleAccessibilityOptions() {
+    this.showAccessibilityOptions = !this.showAccessibilityOptions;
+  }
 }
